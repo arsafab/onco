99f45c08baa3603135c13e5f121b1566677bb4da
arsafab <dmitriibely@gmail.com>
2018-07-27 23:17:38 +0300

    bakdrop closing

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -5,12 +5,20 @@ const closeBtn = document.querySelector('.close-modal');
 const openModal = () => {
     modal.classList.remove('hide');
     modal.classList.add('show');
-}
+};
 
 const closeModal = () => {
     modal.classList.remove('show');
     modal.classList.add('hide');
-}
+};
+
+const backdropClick = (event) => {
+    if (event.target.classList.contains('modal')) {
+        modal.classList.remove('show');
+        modal.classList.add('hide');
+    }
+};
 
 triggers.forEach(trigger => trigger.addEventListener('click', openModal));
 closeBtn.addEventListener('click', closeModal);
+modal.addEventListener('click', backdropClick);
